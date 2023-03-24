c439ac6889326b334942a02d498b3c6cfa2ec0fd
VOJAKIS <valastan1@gmail.com>
2023-03-24 17:48:21 +0100

    Initialized spring-boot project

---
+++ src/main/java/sk/umb/dvestodola/bazarik/BazarikApplication.java
@@ -0,0 +1,13 @@
+package sk.umb.dvestodola.bazarik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarikApplication.class, args);
+	}
+
+}
