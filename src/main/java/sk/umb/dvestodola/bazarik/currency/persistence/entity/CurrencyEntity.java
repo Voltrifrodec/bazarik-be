f1844865376dee18065a6cb166b8991bf17f53a0
VOJAKIS <valastan1@gmail.com>
2023-03-31 20:46:58 +0200

    Added currency layer

---
+++ src/main/java/sk/umb/dvestodola/bazarik/currency/persistence/entity/CurrencyEntity.java
@@ -0,0 +1,45 @@
+package sk.umb.dvestodola.bazarik.currency.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "currency")
+public class CurrencyEntity {
+	@Id
+	@GeneratedValue
+	@Column(name = "id_currency", unique = true)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "symbol", unique = true)
+	private String symbol;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+}
