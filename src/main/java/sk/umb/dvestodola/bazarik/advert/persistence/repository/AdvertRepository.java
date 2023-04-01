19894c549d1d597dcfd9c7a870c0e52399ccb39c
VOJAKIS <valastan1@gmail.com>
2023-04-01 15:21:48 +0200

    Added @Repository annotation

--- src/main/java/sk/umb/dvestodola/bazarik/advert/persistence/repository/AdvertRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/advert/persistence/repository/AdvertRepository.java
@@ -1,9 +1,11 @@
 package sk.umb.dvestodola.bazarik.advert.persistence.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import sk.umb.dvestodola.bazarik.advert.persistence.entity.AdvertEntity;
 
+@Repository
 public interface AdvertRepository extends CrudRepository<AdvertEntity, Long> {
 	Iterable<AdvertEntity> findAllAdvertsByCategoryId(Long categoryId);
 	Iterable<AdvertEntity> findAllAdvertsBySubcategoryId(Long subcategoryId);
--- src/main/java/sk/umb/dvestodola/bazarik/subsubcategory/persistence/repository/SubsubcategoryRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/subsubcategory/persistence/repository/SubsubcategoryRepository.java
@@ -1,9 +1,11 @@
 package sk.umb.dvestodola.bazarik.subsubcategory.persistence.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import sk.umb.dvestodola.bazarik.subsubcategory.persistence.entity.SubsubcategoryEntity;
 
+@Repository
 public interface SubsubcategoryRepository extends CrudRepository<SubsubcategoryEntity, Long> {
 	Iterable<SubsubcategoryEntity> getAllBySubcategoryId(Long subcategoryId);
 
