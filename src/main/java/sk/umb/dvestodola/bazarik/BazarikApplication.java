6025a569c44e8e1740f9ae873e9f785fb768797b
V0ltaroo <nendorans@gmail.com>
2023-04-14 19:22:16 +0200

    temporary branch

--- src/main/java/sk/umb/dvestodola/bazarik/BazarikApplication.java
+++ src/main/java/sk/umb/dvestodola/bazarik/BazarikApplication.java
@@ -1,13 +1,21 @@
 package sk.umb.dvestodola.bazarik;
 
+import java.util.ArrayList;
+
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sk.umb.dvestodola.bazarik.category.persistence.entity.CategoryEntity;
+import sk.umb.dvestodola.bazarik.category.persistence.repository.CategoryRepository;
 import sk.umb.dvestodola.bazarik.currency.persistence.entity.CurrencyEntity;
 import sk.umb.dvestodola.bazarik.currency.persistence.repository.CurrencyRepository;
+import sk.umb.dvestodola.bazarik.subcategory.persistence.entity.SubcategoryEntity;
+import sk.umb.dvestodola.bazarik.subcategory.persistence.repository.SubcategoryRepository;
+import sk.umb.dvestodola.bazarik.subsubcategory.persistence.entity.SubsubcategoryEntity;
+import sk.umb.dvestodola.bazarik.subsubcategory.persistence.repository.SubsubcategoryRepository;
 
 @SpringBootApplication
 public class BazarikApplication implements CommandLineRunner {
@@ -15,6 +23,38 @@ public class BazarikApplication implements CommandLineRunner {
 	@Autowired
 	CurrencyRepository currencyRepository;
 	
+    @Autowired
+    CategoryRepository categoryRepository;
+    
+    @Autowired
+    SubcategoryRepository subcategoryRepository;
+    
+    @Autowired
+    SubsubcategoryRepository subsubcategoryRepository;
+    
+    
+    String[] categories = new String[]{
+        "Autá", "Ovocie", "Elektronika", "Zvieratá",
+        "Oblečenie", "Šport", "Knihy", "Hudba",
+        "Práca a Služby", "Škola", "Stroje", "Pre deti",
+        "Ostatné"  
+    };
+
+    String[] subcategories = new String[]{
+        // Kategória: Autá
+        "Škoda", "Mercedes-Benz", "Opel", "Nissan",
+        "Renault", "Volkswagen", "Fiat", "Lada",
+        "Kia", "Audi", "Dacia", "Ostatné"
+        //TODO: Pridať sekcie pre ostatné kategórie
+    };
+    String[] subsubcategories = new String[]{
+        // Podategória: Autá
+        "Staršie ako 2000", "Novšie ako 2000", "Pre rodiny", "Pre luxus",
+        "Veterány", "Pracovné", "Ostatné"
+        //TODO: Pridať sekcie pre ostatné podkategórie
+    };
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(BazarikApplication.class, args);
 	}
@@ -24,6 +64,12 @@ public class BazarikApplication implements CommandLineRunner {
 		if (currencyRepository.count() == 0) {
 			insertToCurrencies("€", "Euro");
 		}
+        if(categoryRepository.count() == 0) { // TODO: Zmeniť na == 0
+            for(String category : categories) {
+                System.out.println("Inserting category: " + category);
+                insertToCategoriesAndSections(category);
+            }
+        }
 	}
 
 	public void insertToCurrencies(String symbol, String name) {
@@ -35,4 +81,30 @@ public class BazarikApplication implements CommandLineRunner {
 		currencyEntity.setSymbol(symbol);
 		currencyRepository.save(currencyEntity);
 	}
+
+    public void insertToCategoriesAndSections(String categoryName) {
+        if(Strings.isBlank(categoryName))
+            return;
+        
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setName(categoryName);
+        categoryRepository.save(categoryEntity);
+
+        // for(String subcategory : subcategories) {
+        //     SubcategoryEntity subcategoryEntity = new SubcategoryEntity();
+        //     // subcategoryEntity.setCategory(categoryEntity);
+        //     subcategoryEntity.setName(subcategory);
+            
+        //     // for(String subsubcategory : subsubcategories) {
+        //     //     SubsubcategoryEntity subsubcategoryEntity = new SubsubcategoryEntity();
+        //     //     subsubcategoryEntity.setCategory(subcategoryEntity);
+        //     //     subsubcategoryEntity.setName(subsubcategory);
+        //     //     subsubcategoryRepository.save(subsubcategoryEntity);
+        //     // }
+
+        //     subcategoryRepository.save(subcategoryEntity);
+        // }
+
+    }
+
 }
