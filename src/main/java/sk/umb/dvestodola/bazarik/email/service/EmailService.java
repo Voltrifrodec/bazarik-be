2f307961984e2e3ce7991fa326d2e3019bd7d6cc
VOJAKIS <valastan1@gmail.com>
2023-04-18 00:15:27 +0200

    Created sendEmail method

--- src/main/java/sk/umb/dvestodola/bazarik/email/service/EmailService.java
+++ src/main/java/sk/umb/dvestodola/bazarik/email/service/EmailService.java
@@ -1,5 +1,26 @@
 package sk.umb.dvestodola.bazarik.email.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
-	
+	@Autowired
+	private JavaMailSender mailSender;
+
+	public void sendEmail(String toEmailAdress, String subject, String body) {
+		SimpleMailMessage mail = new SimpleMailMessage();
+
+		// TODO: Doplniť e-mailovú adresu
+		mail.setFrom("");
+		mail.setTo(toEmailAdress);
+		mail.setText(body);
+		mail.setSubject(subject);
+
+		mailSender.send(mail);
+
+		System.out.println("E-mail was sent successfully to " + toEmailAdress);
+	}
 }
