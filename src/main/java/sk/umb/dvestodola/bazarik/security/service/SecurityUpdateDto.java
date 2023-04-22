6b76a0c3264bfb36f5f830bd39e97670751f8885
VOJAKIS <valastan1@gmail.com>
2023-04-22 19:41:22 +0200

    Added SecurityUpdateDto

---
+++ src/main/java/sk/umb/dvestodola/bazarik/security/service/SecurityUpdateDto.java
@@ -0,0 +1,30 @@
+package sk.umb.dvestodola.bazarik.security.service;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class SecurityUpdateDto {
+	@NotBlank(message = "Advert id must not me blank.")
+	private String advertId;
+
+	@NotBlank
+	private String email;
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAdvertId() {
+		return advertId;
+	}
+
+	public void setAdvertId(String advertId) {
+		this.advertId = advertId;
+	}
+}
