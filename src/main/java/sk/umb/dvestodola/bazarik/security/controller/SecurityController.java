3c80059027eac89a48ea960141992018145baf22
VOJAKIS <valastan1@gmail.com>
2023-04-22 19:41:06 +0200

    Changed url endpoints, changed to SecurityUpdateDto

--- src/main/java/sk/umb/dvestodola/bazarik/security/controller/SecurityController.java
+++ src/main/java/sk/umb/dvestodola/bazarik/security/controller/SecurityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import sk.umb.dvestodola.bazarik.security.service.SecurityRequestDto;
 import sk.umb.dvestodola.bazarik.security.service.SecurityService;
+import sk.umb.dvestodola.bazarik.security.service.SecurityUpdateDto;
 import sk.umb.dvestodola.bazarik.advert.service.AdvertRequestDto;
 
 @RestController
@@ -19,16 +20,16 @@ public class SecurityController {
 		this.securityService = securityService;
 	}
 
-	@PostMapping("/api/security/hash")
+	@PostMapping("/api/security/create")
 	public String createHashFromAdvert(@Valid @RequestBody AdvertRequestDto advertRequest) {
 		System.out.println("Create hash for advert create was called");
 		return this.securityService.createHashFromAdvert(advertRequest);
 	}
 
-	@PostMapping("/api/security/hash")
-	public String createHashForUpdate(@RequestBody UUID advertId) {
-		System.out.println("Create hash for advert update was called, " + advertId);
-		return this.securityService.createHashForUpdate(advertId);
+	@PostMapping("/api/security/update")
+	public String createHashForUpdate(@RequestBody SecurityUpdateDto securityUpdateDto) {
+		System.out.println("Create hash for advert update was called, " + securityUpdateDto.getAdvertId());
+		return this.securityService.createHashForUpdate(securityUpdateDto);
 	}
 
 	@PostMapping("/api/security/check")
