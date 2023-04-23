0bf0cd7bfe8bb9058ab1556f4f2a8ea16b7d7e92
VOJAKIS <valastan1@gmail.com>
2023-04-23 03:29:53 +0200

    Added NotNull annotation

--- src/main/java/sk/umb/dvestodola/bazarik/security/service/SecurityRequestDto.java
+++ src/main/java/sk/umb/dvestodola/bazarik/security/service/SecurityRequestDto.java
@@ -1,12 +1,15 @@
 package sk.umb.dvestodola.bazarik.security.service;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class SecurityRequestDto {
 	@NotBlank(message = "Code must not be blank.")
+	@NotNull(message = "Code must not be null.")
 	private String code;
 
 	@NotBlank(message = "Hash must not be blank.")
+	@NotNull(message = "Hash must not be null.")
 	private String hash;
 
 	public String getCode() {
