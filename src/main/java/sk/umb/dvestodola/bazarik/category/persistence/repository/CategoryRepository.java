9ddea7b917355ce7e915d4e6504a63fcc85ef177
VOJAKIS <valastan1@gmail.com>
2023-05-17 14:49:49 +0200

    repository query override findall

--- src/main/java/sk/umb/dvestodola/bazarik/category/persistence/repository/CategoryRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/category/persistence/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
 package sk.umb.dvestodola.bazarik.category.persistence.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
