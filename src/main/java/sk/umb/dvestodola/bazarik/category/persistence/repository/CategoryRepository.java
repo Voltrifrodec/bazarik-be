aaf00d862a2796febd540c6009cb0e1d07e390a0
VOJAKIS <valastan1@gmail.com>
2023-05-17 14:49:38 +0200

    repository query override findall

--- src/main/java/sk/umb/dvestodola/bazarik/category/persistence/repository/CategoryRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/category/persistence/repository/CategoryRepository.java
@@ -7,5 +7,19 @@ import sk.umb.dvestodola.bazarik.category.persistence.entity.CategoryEntity;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<CategoryEntity, Long> {
-	
+	@Override
+	@Query(value = """
+		SELECT c.id_category, c.name, COUNT(a.id_advert) AS number_of_adverts
+		FROM category c
+		LEFT JOIN advert a ON a.id_category  = c.id_category
+		GROUP BY c.id_category
+
+		UNION
+
+		SELECT c.id_category, c.name, COUNT(a.id_advert) AS number_of_adverts
+		FROM advert a
+		LEFT JOIN category c ON a.id_category  = c.id_category
+		GROUP BY c.id_category
+	""", nativeQuery = true)
+	Iterable<CategoryEntity> findAll();
 }
