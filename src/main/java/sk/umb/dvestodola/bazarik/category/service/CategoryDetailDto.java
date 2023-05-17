181399438b7c9f05d3e37f30b82bb5293b6cfac7
VOJAKIS <valastan1@gmail.com>
2023-05-17 14:51:21 +0200

    added attribute numberOfAdverts to categoryDetail w/methods

--- src/main/java/sk/umb/dvestodola/bazarik/category/service/CategoryDetailDto.java
+++ src/main/java/sk/umb/dvestodola/bazarik/category/service/CategoryDetailDto.java
@@ -3,8 +3,14 @@ package sk.umb.dvestodola.bazarik.category.service;
 public class CategoryDetailDto {
 	private Long id;
 	private String name;
-
+	private Long numberOfAdverts;
 	
+	public Long getNumberOfAdverts() {
+		return numberOfAdverts;
+	}
+	public void setNumberOfAdverts(Long numberOfAdverts) {
+		this.numberOfAdverts = numberOfAdverts;
+	}
 	public Long getId() {
 		return id;
 	}
