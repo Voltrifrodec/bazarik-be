9e5e530314a6db651dc4b51d4b4e67a17b80e869
VOJAKIS <valastan1@gmail.com>
2023-05-18 03:39:30 +0200

    Added security detail dto

---
+++ src/main/java/sk/umb/dvestodola/bazarik/security/service/SecurityDetailDto.java
@@ -0,0 +1,13 @@
+package sk.umb.dvestodola.bazarik.security.service;
+
+public class SecurityDetailDto {
+	private String hash;
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+}
