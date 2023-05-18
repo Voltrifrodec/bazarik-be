a773b236ad3f54aa1b47a2500ca6c468c395c02f
VOJAKIS <valastan1@gmail.com>
2023-05-18 23:31:06 +0200

    Added repository methods

--- src/main/java/sk/umb/dvestodola/bazarik/advert/persistence/repository/AdvertPageRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/advert/persistence/repository/AdvertPageRepository.java
@@ -10,4 +10,8 @@ import sk.umb.dvestodola.bazarik.advert.persistence.entity.AdvertEntity;
 @Repository
 public interface AdvertPageRepository extends PagingAndSortingRepository<AdvertEntity, Long> {
 	Page<AdvertEntity> findAll(Pageable pageable);
+
+	Page<AdvertEntity> findAllByCategoryId(Long categoryId, Pageable pageable);
+	Page<AdvertEntity> findAllBySubcategoryId(Long subcategoryId, Pageable pageable);
+	Page<AdvertEntity> findAllBySubsubcategoryId(Long subsubcategoryId, Pageable pageable);
 }
