e98cd27f46e251a4efaf2a26f48a640f49ebe25f
VOJAKIS <valastan1@gmail.com>
2023-05-19 02:46:18 +0200

    Added getImageIds function

--- docs/data_generating/generator.py
+++ docs/data_generating/generator.py
@@ -4,23 +4,32 @@ import sys
 
 # Connect to MariaDB Platform
 try:
-    connection = mariadb.connect(
-        user="root",
-        password="root",
-        host="localhost",
-        port=3306,
-        database="bazarik"
-    )
+	connection = mariadb.connect(
+		user="root",
+		password="root",
+		host="localhost",
+		port=3306,
+		database="bazarik"
+	)
 except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+	print(f"Error connecting to MariaDB Platform: {e}")
+	sys.exit(1)
 
 # Get Cursor
 cursor = connection.cursor()
 
-cursor.execute('''
-	SELECT id, original_size_bytes, size_bytes FROM image
-''')
+# Get all image ids
+def getImageIds():
+	try:
+		cursor.execute('SELECT id FROM image')
+		return [id[0] for (id) in cursor]
+	except mariadb.Error as e:
+		print(f'Error: {e}')
 
-for (id, original_size_bytes, size_bytes) in cursor:
-    print(f'id: {id}, original_size_bytes: {original_size_bytes:6}, size_bytes: {size_bytes:6}, diff: {((original_size_bytes - size_bytes) / 1000):6}kB')
+
+imageIdsSet = set(getImageIds())
+print(imageIdsSet)
+
+# for (id, original_size_bytes, size_bytes) in cursor:
+# 	print(f'id: {id}, original_size_bytes: {original_size_bytes:6}, size_bytes: {size_bytes:6}, diff: {((original_size_bytes - size_bytes) / 1000):6}kB')
+
