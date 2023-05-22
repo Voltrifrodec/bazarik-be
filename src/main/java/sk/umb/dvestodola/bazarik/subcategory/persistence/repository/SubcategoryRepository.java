8073433fa51e740bb40408c31c6dc5893437c698
VOJAKIS <valastan1@gmail.com>
2023-05-22 15:22:33 +0200

    Added subcategory findAll implementation

--- src/main/java/sk/umb/dvestodola/bazarik/subcategory/persistence/repository/SubcategoryRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/subcategory/persistence/repository/SubcategoryRepository.java
@@ -10,13 +10,19 @@ import sk.umb.dvestodola.bazarik.subcategory.persistence.entity.SubcategoryEntit
 @Repository
 public interface SubcategoryRepository extends CrudRepository<SubcategoryEntity, Long> {
 	@Query(value = """
-		SELECT s.id_subcategory, s.subcategory_name, COUNT(a.id_advert) AS subcategory_number_of_adverts, c.id_category, c.name, c.emoji, COUNT(a.id_advert) AS number_of_adverts
-		FROM subcategory s
-		INNER JOIN category_subcategory cs ON cs.id_subcategory = s.id_subcategory
-		LEFT JOIN category c ON c.id_category = cs.id_category
-		LEFT JOIN advert a ON a.id_subcategory = s.id_subcategory
-		WHERE cs.id_category = :categoryId
-		GROUP BY cs.id_subcategory
+		SELECT
+			sub.id_subcategory, sub.subcategory_name,
+			COUNT(a.id_advert) AS subcategory_number_of_adverts,
+			c.id_category, c.category_name, c.category_emoji, COUNT(a.id_advert) AS category_number_of_adverts
+		
+		FROM subcategory sub
+			INNER JOIN category_subcategory cs ON cs.id_subcategory = sub.id_subcategory
+			LEFT JOIN category c ON c.id_category = cs.id_category
+			LEFT JOIN advert a ON a.id_subcategory = sub.id_subcategory
+
+		WHERE c.id_category = :categoryId
+
+		GROUP BY sub.id_subcategory
 	""", nativeQuery = true)
 	Iterable<SubcategoryEntity> getAllByCategoryId(@Param("categoryId") Long categoryId);
 }
