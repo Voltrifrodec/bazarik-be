56458f447ef7c55d2f5403c8f3d1f9dc2ca9ab4b
VOJAKIS <valastan1@gmail.com>
2023-05-22 02:00:14 +0200

    Added repository query method for number of adverts in subsubcategory by subcategory id

--- src/main/java/sk/umb/dvestodola/bazarik/subsubcategory/persistence/repository/SubsubcategoryRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/subsubcategory/persistence/repository/SubsubcategoryRepository.java
@@ -1,6 +1,8 @@
 package sk.umb.dvestodola.bazarik.subsubcategory.persistence.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import sk.umb.dvestodola.bazarik.subsubcategory.persistence.entity.SubsubcategoryEntity;
