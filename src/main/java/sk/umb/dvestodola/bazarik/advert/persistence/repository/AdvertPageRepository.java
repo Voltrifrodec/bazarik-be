9699b9c0c2e5924859d870b049ec7a6306f479d4
VOJAKIS <valastan1@gmail.com>
2023-05-23 15:51:23 +0200

    Added possible repository implementation without custom query

--- src/main/java/sk/umb/dvestodola/bazarik/advert/persistence/repository/AdvertPageRepository.java
+++ src/main/java/sk/umb/dvestodola/bazarik/advert/persistence/repository/AdvertPageRepository.java
@@ -19,4 +19,7 @@ public interface AdvertPageRepository extends PagingAndSortingRepository<AdvertE
 
 	@Query(value = "SELECT a FROM advert a WHERE LOWER(a.name) LIKE %:query% OR LOWER(a.description) LIKE %:query%")
 	Page<AdvertEntity> findAllByQuery(@Param("query") String query, Pageable pageable);
+
+	// https://www.bezkoder.com/jpa-repository-query/
+	Page<AdvertEntity> findByNameContainingOrDescriptionContainingIgnoreCase(String name, String description, Pageable pageable);
 }
